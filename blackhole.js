6a6dc9584545a8835129c383a1ee1e06f9e113bd
Kura <kura.hax@gmail.com>
2021-10-14 14:38:29 +0100

    X-Clacks

--- blackhole.js
+++ blackhole.js
@@ -29,6 +29,7 @@ async function handle_req(request) {
     new_headers.set("Referrer-Policy", "strict-origin-when-cross-origin")
     new_headers.set("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()")
     new_headers.set("Content-Security-Policy", "default-src 'self'; script-src 'self' a.disquscdn.com disqus.com syslogtv.disqus.com gist.github.com; style-src 'self' assets-cdn.github.com netdna.bootstrapcdn.com a.disquscdn.com; img-src 'self' referrer.disqus.com a.disquscdn.com img.shields.io; font-src 'self' data: netdna.bootstrapcdn.com; connect-src 'none'; media-src 'self'; object-src 'self' player.vimeo.com; child-src www.youtube.com player.vimeo.com disqus.com; frame-ancestors 'none'; form-action 'none'; upgrade-insecure-requests; base-uri https://kura.gg; manifest-src 'none';")
+    new_headers.set("X-Clacks-Overhead", "GNU Terry Pratchett")
 
     // This causes the worker to error, not sure why
     // if (content_type.startsWith("text/html")) {
--- headers.js
+++ headers.js
@@ -16,6 +16,7 @@ async function handle_equest(request) {
   new_headers.set("Referrer-Policy", "strict-origin-when-cross-origin")
   new_headers.set("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()")
   new_headers.set("Content-Security-Policy", "default-src 'self'; script-src 'self' a.disquscdn.com disqus.com syslogtv.disqus.com gist.github.com; style-src 'self' assets-cdn.github.com netdna.bootstrapcdn.com a.disquscdn.com; img-src 'self' referrer.disqus.com a.disquscdn.com img.shields.io; font-src 'self' data: netdna.bootstrapcdn.com; connect-src 'none'; media-src 'self'; object-src 'self' player.vimeo.com; child-src www.youtube.com player.vimeo.com disqus.com; frame-ancestors 'none'; form-action 'none'; upgrade-insecure-requests; base-uri https://kura.gg; manifest-src 'none';")
+  new_headers.set("X-Clacks-Overhead", "GNU Terry Pratchett")
 
   return new Response(response.body, {
     status: response.status,
