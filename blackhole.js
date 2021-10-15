ad8db6849914acdd495ca6358773b4ddbe084f30
Kura <kura.hax@gmail.com>
2021-10-15 18:13:45 +0100

    Remove some upstream headers

--- blackhole.js
+++ blackhole.js
@@ -4,18 +4,31 @@ class ElementHandler {
   }
 }
 
+let remove_headers = [
+  "age",
+  "via",
+  "x-cache",
+  "x-cache-hits",
+  "x-fastly-request-id",
+  "x-frame-options",
+  "x-github-request-id",
+  "x-proxy-cache",
+  "x-served-by",
+  "x-timer"
+]
+
 async function handle_req(request) {
-  var old_url = new URL(request.url)
+  let old_url = new URL(request.url)
   if (old_url == "https://kura.gg/blackhole" || old_url == "https://kura.gg/blackhole/") {
     return Response.redirect("https://kura.gg/blackhole/index.html", 301)
   } else {
-    var new_url = old_url.toString().replace(
+    let new_url = old_url.toString().replace(
       "https://kura.gg/blackhole",
       "https://kura.github.io/blackhole"
     )
 
-    const res = await fetch(new_url)
-    const content_type = res.headers.get("Content-Type")
+    let res = await fetch(new_url)
+    let content_type = res.headers.get("Content-Type")
 
     let new_headers = new Headers(res.headers)
     new_headers.set("Access-Control-Allow-Headers", "Origin,Range,Accept-Encoding,Referer")
@@ -30,6 +43,10 @@ async function handle_req(request) {
     new_headers.set("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()")
     new_headers.set("Content-Security-Policy", "default-src 'self'; script-src 'self' a.disquscdn.com disqus.com syslogtv.disqus.com gist.github.com; style-src 'self' assets-cdn.github.com netdna.bootstrapcdn.com a.disquscdn.com; img-src 'self' referrer.disqus.com a.disquscdn.com img.shields.io; font-src 'self' data: netdna.bootstrapcdn.com; connect-src 'none'; media-src 'self'; object-src 'self' player.vimeo.com; child-src www.youtube.com player.vimeo.com disqus.com; frame-ancestors 'none'; form-action 'none'; upgrade-insecure-requests; base-uri https://kura.gg; manifest-src 'none';")
     new_headers.set("X-Clacks-Overhead", "GNU Terry Pratchett")
+
+    remove_headers.forEach(function(name){
+      new_headers.delete(name)
+    })
 
     // This causes the worker to error, not sure why
     // if (content_type.startsWith("text/html")) {
--- headers.js
+++ headers.js
@@ -2,6 +2,19 @@ addEventListener('fetch', event => {
   event.respondWith(handle_equest(event.request))
 })
 
+let remove_headers = [
+  "age",
+  "via",
+  "x-cache",
+  "x-cache-hits",
+  "x-fastly-request-id",
+  "x-frame-options",
+  "x-github-request-id",
+  "x-proxy-cache",
+  "x-served-by",
+  "x-timer"
+]
+
 async function handle_equest(request) {
   let response = await fetch(request)
   let new_headers = new Headers(response.headers)
@@ -17,6 +30,11 @@ async function handle_equest(request) {
   new_headers.set("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()")
   new_headers.set("Content-Security-Policy", "default-src 'self'; script-src 'self' a.disquscdn.com disqus.com syslogtv.disqus.com gist.github.com; style-src 'self' assets-cdn.github.com netdna.bootstrapcdn.com a.disquscdn.com; img-src 'self' referrer.disqus.com a.disquscdn.com img.shields.io; font-src 'self' data: netdna.bootstrapcdn.com; connect-src 'none'; media-src 'self'; object-src 'self' player.vimeo.com; child-src www.youtube.com player.vimeo.com disqus.com; frame-ancestors 'none'; form-action 'none'; upgrade-insecure-requests; base-uri https://kura.gg; manifest-src 'none';")
   new_headers.set("X-Clacks-Overhead", "GNU Terry Pratchett")
+
+
+  remove_headers.forEach(function(name){
+    new_headers.delete(name)
+  })
 
   return new Response(response.body, {
     status: response.status,
