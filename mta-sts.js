1fd404a54b563c22dfa038eb4a31289ea920ce5e
Kura <kura.hax@gmail.com>
2023-07-30 13:15:59 +0100

    Update mta-sts

--- mta-sts.js
+++ mta-sts.js
@@ -1,6 +1,6 @@
-let content = [
+let kuragg_content = [
   "version: STSv1",
-  "mode: testing",
+  "mode: enforce",
   "mx: mx1.smtp.goog",
   "mx: mx2.smtp.goog",
   "mx: mx3.smtp.goog",
@@ -9,14 +9,32 @@ let content = [
   ""
 ].join("\n")
 
+let aliaskuraxyz_content = [
+  "version: STSv1",
+  "mode: enforce",
+  "mx: inbound-smtp.eu-west-1.amazonaws.com",
+  "max_age: 604800",
+  ""
+].join("\n")
+
 async function handle_req(request) {
   let url = new URL(request.url)
   if (url.pathname == "/.well-known/mta-sts.txt") {
-    return new Response(content, {
-      headers: {
-        "Content-Type": "text/plain; charset=UTF-8",
-      }
-    })
+    if (url.host == "mta-sts.aliaskura.xyz") {
+      return new Response(aliaskuraxyz_content, {
+        headers: {
+          "Content-Type": "text/plain; charset=UTF-8",
+          "X-Clacks-Overhead": "GNU Terry Pratchett"
+        }
+      })
+    } else {
+      return new Response(kuragg_content, {
+        headers: {
+          "Content-Type": "text/plain; charset=UTF-8",
+          "X-Clacks-Overhead": "GNU Terry Pratchett"
+        }
+      })
+    }
   } else {
     return new Response("", {
       status: 404
